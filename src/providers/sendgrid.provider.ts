150a0db5ef6721855223ec6c2c6102dfeb6e0bb6
Ruben Bautista Mendoza <oxigeno78@gmail.com>
2026-01-13 14:46:16 -0600

    Configuración adicional para SESv3... factoria

--- src/providers/sendgrid.provider.ts
+++ src/providers/sendgrid.provider.ts
@@ -2,17 +2,17 @@ import { EmailProvider, SendEmailOptions } from "../interfaces/email.interfaces"
 import sgMail from "@sendgrid/mail";
 
 export class SendGridProvider implements EmailProvider {
-  constructor(apiKey: string) {
-    sgMail.setApiKey(apiKey);
-  }
+    constructor(apiKey: string) {
+        sgMail.setApiKey(apiKey);
+    }
 
-  async sendMail(options: SendEmailOptions): Promise<void> {
-    await sgMail.send({
-      to: options.to as any,
-      from: options.from!,
-      subject: options.subject,
-      html: options.html,
-      text: options.text!,
-    });
-  }
+    async sendMail(options: SendEmailOptions): Promise<void> {
+        await sgMail.send({
+            to: options.to as any,
+            from: options.from!,
+            subject: options.subject,
+            html: options.html,
+            text: options.text!,
+        });
+    }
 }
